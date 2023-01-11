dee3db3b98e9aaeb26a1ea94d3c1b57eaeb44ffa
thor-r <twilliamrefoy@gmail.com>
2023-01-11 18:01:04 +0000

    environment ground up

---
+++ README.md
@@ -0,0 +1,25 @@
+useFrame is invoked just before the current frame is rendered to the canvas and with default canvas setting useFrame is called 60 times per second (60fps)
+
+setting !rotate works like a flipswitch between true and false
+
+
+continuity of state ---->
+
+We are not using the <boxGeometry /> tag in the JSX anymore, but instantiating a new THREE.BoxGeometry and appending it to the props in the mesh tag instead.
+
+This appears to work exactly the same. But there is a problem. This is a very small application, so in any case, the problem is likely to go unnoticed. But however, it is there.
+
+This Box function will be re-executed every time there is a state change. This means that it will unnecessarily re-create a new THREE.BoxGeometry() every time it's called.
+
+Click on a Box and you will see that its geometry's uuid is changing. This means that it is a new instance of a THREE.BoxGeometry in memory and being rendered to the scene. Creating new objects unnecessarily is not recommended.
+
+We can solve this problem by using the useMemo hook. useMemo will act as cache for the geometry and return that instead of generating a new object.
+
+useMemo will cause the inner function to only run when needed. Use useMemo when you want to keep expensive, resource intensive functions from needlessly re-running.
+
+
+modulus ----> onPointerDown={() => setCount((count + 1) % 2)} //modulus 2 means it will either be a zero or 1 (0 + 1 / 2 remainder 1, 1 + 1 / 2 remainder 0)
+
+geometry can be added as a prop to a mesh or a jsx tag inside mesh
+
+lights lesson - to create a sun use a sphere inside your light source tag
---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import {
+  Stats,
+  OrbitControls,
+  Environment,
+  ContactShadows,
+} from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Leva, useControls } from 'leva'
+
+function Model() {
+  const { scene } = useLoader(GLTFLoader, './models/scene.glb')
+
+  const {
+    x,
+    y,
+    z,
+    visible,
+    color,
+    metalness,
+    roughness,
+    clearcoat,
+    clearcoatRoughness,
+    transmission,
+    ior,
+    thickness,
+  } = useControls('Suzanne', {
+    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    visible: true,
+    color: { value: '#ffbc85' },
+    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
+    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
+    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
+    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
+  })
+
+  return (
+    <primitive
+      object={scene}
+      children-0-rotation={[x, y, z]}
+      children-0-visible={visible}
+      children-0-material-color={color}
+      children-0-material-metalness={metalness}
+      children-0-material-roughness={roughness}
+      children-0-material-clearcoat={clearcoat}
+      children-0-material-clearcoatRoughness={clearcoatRoughness}
+      children-0-material-transmission={transmission}
+      children-0-material-ior={ior}
+      children-0-material-thickness={thickness}
+    />
+  )
+}
+
+function Env() {
+  const { height, radius, scale } = useControls('Ground', {
+    height: { value: 48, min: 0, max: 100, step: 1 },
+    radius: { value: 107, min: 0, max: 1000, step: 1 },
+    scale: { value: 100, min: 0, max: 1000, step: 1 },
+  })
+  return (
+    <Environment
+      // preset="sunset"
+      // files='./img/garden_nook_4k.hdr'
+      files='./img/desert.hdr'
+      background
+      ground={{
+        height: height,
+        radius: radius,
+        scale: scale,
+      }}
+    />
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [-8, 5, 8] }}>
+        <Env />
+        <Model />
+        <ContactShadows
+          scale={150}
+          position={[0.33, -0.33, 0.33]}
+          opacity={1.5}
+        />
+        <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
+        <Stats />
+      </Canvas>
+      <Leva collapsed />
+    </>
+  )
+}
